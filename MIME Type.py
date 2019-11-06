4127e41a8109660254716b19b5cffc6e35cff7b7
VetusLudaeus <oleg.matsvay@gmail.com>
2019-11-06 22:17:10 +0500

    Решение задачи MIME Type

---
+++ MIME Type.py
@@ -0,0 +1,21 @@
+#https://www.codingame.com/ide/puzzle/mime-type
+
+table = {}
+
+n = int(input())  # Number of elements which make up the association table.
+q = int(input())  # Number Q of file names to be analyzed.
+for i in range(n):
+    # ext: file extension
+    # mt: MIME type.
+    ext, mt = input().split()
+    table[ext.lower()] = mt
+for i in range(q):
+    fname = input().split('.') # One file name per line.
+
+    if len(fname) > 1:
+        try:
+            print(table[fname[-1].lower()])
+        except KeyError:
+            print("UNKNOWN")
+    else:
+        print("UNKNOWN")
